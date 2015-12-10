748ba86efe49b4b170b66d54f01c2e36d556e19b
klsmith-usgs <klsmith@usgs.gov>
2015-12-10 09:03:30 -0600

    initial

---
+++ extent-validator.py
@@ -0,0 +1,18 @@
+from dbconnect import DBConnect
+
+
+class ExtentValidator(object):
+    def __init__(self):
+        pass
+
+    def validate_frame(self):
+        pass
+
+    def validate_scenes(self):
+        pass
+
+    def db_scenes(self):
+        pass
+
+    def comp_scenes(self):
+        pass
